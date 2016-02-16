2c0539ccc0d962a51ba42404bf0b8efea973a53d
Ashwin Rao <theonlyrao@gmail.com>
2016-02-15 23:23:01 -0700

    tests in shape but not passing; still need to build print methods

--- night_writer/lib/night_write.rb
+++ night_writer/lib/night_write.rb
@@ -2,152 +2,105 @@ require 'pry'
 
 class NightWriter
 
-  def initialize
-    @english_to_braille_hash = Hash.new
+  attr_reader :english_array, :etb_hash
 
-    @english_to_braille_hash["#"] = ["#", "#", "#", "#", "#", "#"]
-    @english_to_braille_hash["0"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["1"] = ["1", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["2"] = ["2", ".", "0", ".", ".", "."]
-    @english_to_braille_hash["3"] = ["3", "0", ".", ".", ".", "."]
-    @english_to_braille_hash["4"] = ["4", "0", ".", "0", ".", "."]
-    @english_to_braille_hash["5"] = ["5", ".", ".", "0", ".", "."]
-    @english_to_braille_hash["6"] = ["6", "0", "0", ".", ".", "."]
-    @english_to_braille_hash["7"] = ["7", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["8"] = ["8", ".", "0", "0", ".", "."]
-    @english_to_braille_hash["9"] = ["9", "0", "0", ".", ".", "."]
-    @english_to_braille_hash["a"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["b"] = [".", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["c"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["d"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["e"] = ["e", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["f"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["g"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["h"] = [".", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["i"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["j"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["k"] = [".", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["l"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["m"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["n"] = [".", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["o"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["p"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["q"] = [".", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["r"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["s"] = ["s", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["t"] = ["t", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["u"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["v"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["w"] = [".", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash["x"] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["y"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["z"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["!"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["'"] = [".", "0", ".", "0", "0", "0"]
-    @english_to_braille_hash[","] = [".", "0", "0", "0", ".", "."]
-    @english_to_braille_hash["-"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["."] = ["p", "e", "r", "i", "o","d"]
-    @english_to_braille_hash["?"] = ["0", ".", ".", ".", ".", "."]
-    @english_to_braille_hash["Capital"] = ["C", "C", "C", "C", "C", "C"]
-    @english_to_braille_hash[" "] = ["s", "p", "a", "a", "c", "e"]
+  def initialize
+    @etb_hash = Hash.new
+
+    @etb_hash["#"] = [".", "0", ".", "0", "0", "0"]
+    @etb_hash["0"] = [".", "0", "0", "0", ".", "."]
+    @etb_hash["1"] = ["0", ".", ".", ".", ".", "."]
+    @etb_hash["2"] = ["0", ".", "0", ".", ".", "."]
+    @etb_hash["3"] = ["0", "0", ".", ".", ".", "."]
+    @etb_hash["4"] = ["0", "0", ".", "0", ".", "."]
+    @etb_hash["5"] = ["0", ".", ".", "0", ".", "."]
+    @etb_hash["6"] = ["0", "0", "0", ".", ".", "."]
+    @etb_hash["7"] = ["0", "0", "0", "0", ".", "."]
+    @etb_hash["8"] = ["0", ".", "0", "0", ".", "."]
+    @etb_hash["9"] = [".", "0", "0", ".", ".", "."]
+    @etb_hash["a"] = ["0", ".", ".", ".", ".", "."]
+    @etb_hash["b"] = ["0", ".", "0", ".", ".", "."]
+    @etb_hash["c"] = ["0", "0", ".", ".", ".", "."]
+    @etb_hash["d"] = ["0", "0", ".", "0", ".", "."]
+    @etb_hash["e"] = ["0", ".", ".", "0", ".", "."]
+    @etb_hash["f"] = ["0", "0", "0", ".", ".", "."]
+    @etb_hash["g"] = ["0", "0", "0", "0", ".", "."]
+    @etb_hash["h"] = ["0", ".", "0", "0", ".", "."]
+    @etb_hash["i"] = [".", "0", "0", ".", ".", "."]
+    @etb_hash["j"] = [".", "0", "0", "0", ".", "."]
+    @etb_hash["k"] = ["0", ".", ".", ".", "0", "."]
+    @etb_hash["l"] = ["0", ".", "0", ".", "0", "."]
+    @etb_hash["m"] = ["0", "0", ".", ".", "0", "."]
+    @etb_hash["n"] = ["0", "0", ".", "0", "0", "."]
+    @etb_hash["o"] = ["0", ".", ".", "0", "0", "."]
+    @etb_hash["p"] = ["0", "0", "0", ".", "0", "."]
+    @etb_hash["q"] = ["0", "0", "0", "0", "0", "."]
+    @etb_hash["r"] = ["0", ".", "0", "0", "0", "."]
+    @etb_hash["s"] = [".", "0", "0", ".", "0", "."]
+    @etb_hash["t"] = [".", "0", "0", "0", "0", "."]
+    @etb_hash["u"] = ["0", ".", ".", ".", "0", "0"]
+    @etb_hash["v"] = ["0", ".", "0", ".", "0", "0"]
+    @etb_hash["w"] = [".", "0", "0", "0", ".", "0"]
+    @etb_hash["x"] = ["0", "0", ".", ".", "0", "0"]
+    @etb_hash["y"] = ["0", "0", ".", "0", "0", "0"]
+    @etb_hash["z"] = ["0", ".", ".", "0", "0", "0"]
+    @etb_hash["!"] = [".", ".", "0", "0", "0", "."]
+    @etb_hash["'"] = [".", ".", ".", ".", "0", "."]
+    @etb_hash[","] = [".", ".", "0", ".", ".", "."]
+    @etb_hash["-"] = [".", ".", ".", ".", "0", "0"]
+    @etb_hash["."] = [".", ".", "0", "0", ".","0"]
+    @etb_hash["?"] = [".", ".", "0", ".", "0", "0"]
+    @etb_hash["Capital"] = [".", ".", ".", ".", ".", "0"]
+    @etb_hash[" "] = [".", ".", ".", ".", ".", "."]
 
     # input_file = ARGV[0]
     # @output_file = ARGV[1]
-    # @input = File.read(input_file.chomp)
+    # @input = File.read(input_file)
     # send_to_translate(@input)
-    # # File.write(output_file, output_text)
   end
 
   def send_to_translate(input)
-    @english_array = @input.split()
+    @english_array = input.split()
     translate(@english_array)
     p "Message has been translated into braille. Please see output file."
   end
 
-  def translate(array)
-    @braille_sentence = []
-    @braille_1 = []
-    @braille_2 = []
-    @braille_3 = []
-    @translated_words = array.map do |word|
-      if is_number(word)
-        number_translator(word)
-        @braille_sentence << @braille_numbers
+  def translate(english_array)
+    @braille_arrays = []
+    english_array.each do |word|
+      @characters = word.chars
+      end
+      if @characters[0].to_i.to_s == @characters[0]
+        @braille_arrays << @etb_hash["#"]
+        @characters.each do |char|
+          @braille_arrays << @etb_hash[char]
+        end
+        if @braille_arrays[-1] != @etb_hash["."]
+          @braille_arrays << @etb_hash[" "]
+        end
+        # @braille_arrays.unshift(@etb_hash["#"])
       else
-        non_number_translator(word)
-        # @braille_sentence << @braille_characters
-        @braille_1 << @line_1
-        @braille_2 << @line_2
-        @braille_2 << @line_2
+        if @characters.join.capitalize == @characters.join
+          @braille_arrays << [".", ".", ".", ".", ".", "0"]
+          @characters.each do |char|
+            new_char = char.downcase
+            @braille_arrays << @etb_hash[new_char]
+          end
+          if @braille_arrays[-1] != @etb_hash["."]
+            @braille_arrays << @etb_hash[" "]
+          end
+          # @braille_arrays.unshift(@etb_hash["Capital"])
+        else
+          @characters.each do |char|
+            @braille_arrays << @etb_hash[char]
+          end
+          if @braille_arrays[-1] != @etb_hash["."]
+            @braille_arrays << @etb_hash[" "]
+          end
+        end
       end
-    end
-    # @output_text = @braille_sentence.flatten.join
-    output_text(@braille_1, @braille_2, @braille_3)
-
-  end
-
-  def output_text(arg1, arg2, arg3)
-    File.write(@output_file, arg1.flatten.join)
-    File.write(@output_file, arg2.flatten.join)
-    File.write(@output_file, arg3.flatten.join)
-  end
-
-
-  def is_number(word)
-    true if word.to_i.to_s == word
-  end
-
-  def number_translator(numbers)
-    @braille_numbers = []
-    numbers.chars.each do |number|
-      @braille_numbers << @english_to_braille_hash[number]
-    end
-    if @braille_numbers[-1] == ["period"]
-      @braille_numbers
-    else
-      @braille_numbers << @english_to_braille_hash[" "]
-    end
-    @braille_numbers.unshift(@english_to_braille_hash["#"])
-  end
-
-  def non_number_translator(letters)
-    @braille_characters = []
-    if letters == letters.capitalize
-      letters.downcase.chars.each do |letter|
-        @braille_characters << @english_to_braille_hash[letter]
-      end
-      @braille_characters.unshift(@english_to_braille_hash["Capital"])
-    else
-      letters.chars.each do |letter|
-        @braille_characters << @english_to_braille_hash[letter]
-      end
-    end
-    if @braille_characters[-1] == ["p", "e", "r", "i", "o","d"] # FIX THIS WHEN ACTUAL VALUE IS PUT IN HASH!
-      @braille_characters
-    else
-      @braille_characters << @english_to_braille_hash[" "]
-    end
-    @line_1 = []
-    @line_2 = []
-    @line_3 = []
-    @braille_characters.each do |character|
-      braille_formatter(character)
-      @line_1 << @return_1
-      @line_2 << @return_2
-      @line_3 << @return_3
-    end
-  end
-
-  def braille_formatter_1(braille_letter_or_number)
-    @return_1 = braille_letter_or_number[0, 1]
-  end
-
-  def braille_formatter_2(braille_letter_or_number)
-    @return_2 = braille_letter_or_number[2, 3]
-  end
-
-  def braille_formatter_3(braille_letter_or_number)
-    @return_3 = braille_letter_or_number[4, 5]
+    @braille_arrays
+    # binding.pry
   end
 
 end
--- night_writer/test/night_write_test.rb
+++ night_writer/test/night_write_test.rb
@@ -4,27 +4,132 @@ require '../lib/night_write'
 
 class NightWriterTest < Minitest::Test
 
-  def test_braille_formatter_returns_three_lines_of_text
+  def test_send_to_translate_gives_array_of_words_from_input
     test = NightWriter.new
+    input = "hello World 123 goodbye."
 
-    letter = ["a", "b", "c", "d", "e", "f"]
+    array = test.send_to_translate(input)
 
-    line_1 = ["a", "b"]
-    line_2 = ["c", "d"]
-    line_3 = ["e", "f"]
-
-    assert_equal line_1 && line_2 && line_3, test.braille_formatter(letter)
+    assert_equal ["hello", "World", "123", "goodbye."], array
   end
 
-  def test_non_number_translator_returns_three_lines_of_braille
+  def test_translate_of_numbers_in_middle_gives_braille_array
+    test = NightWriter.new
+    input = ["123"]
+
+    pound = [".", "0", ".", "0", "0", "0"]
+    one = ["0", ".", ".", ".", ".", "."]
+    two = ["0", ".", "0", ".", ".", "."]
+    three = ["0", "0", ".", ".", ".", "."]
+    space = [".", ".", ".", ".", ".", "."]
+
+    braille_input = test.translate(input)
+
+    assert_equal [pound, one, two, three, space], braille_input
+  end
+
+  def test_translate_of_lowercase_in_middle_gives_braille_array
+    test = NightWriter.new
+    input = ["hi"]
+
+    braille_input = test.translate(input)
+
+    h = ["0", ".", "0", "0", ".", "."]
+    i = [".", "0", "0", ".", ".", "."]
+    space = [".", ".", ".", ".", ".", "."]
+
+    assert_equal [h, i, space], braille_input
+  end
+
+  def test_translate_of_capital_in_middle_gives_braille_array
+    test = NightWriter.new
+    input = ["Hi"]
+
+    braille_input = test.translate(input)
+
+    capital = [".", ".", ".", ".", ".", "0"]
+    h = ["0", ".", "0", "0", ".", "."]
+    i = [".", "0", "0", ".", ".", "."]
+    space = [".", ".", ".", ".", ".", "."]
+
+    assert_equal [capital, h, i, space], braille_input
+  end
+
+  def test_translate_of_numbers_at_end_gives_braille_array
+    test = NightWriter.new
+    input = ["123."]
+
+    pound = [".", "0", ".", "0", "0", "0"]
+    one = ["0", ".", ".", ".", ".", "."]
+    two = ["0", ".", "0", ".", ".", "."]
+    three = ["0", "0", ".", ".", ".", "."]
+    period = [".", ".", "0", "0", ".","0"]
+
+    braille_input = test.translate(input)
+
+    assert_equal [pound, one, two, three, period], braille_input
+  end
+
+  def test_translate_of_lowercase_at_end_gives_braille_array
+    test = NightWriter.new
+    input = ["hi."]
+
+    braille_input = test.translate(input)
+
+    h = ["0", ".", "0", "0", ".", "."]
+    i = [".", "0", "0", ".", ".", "."]
+    period = [".", ".", "0", "0", ".","0"]
+
+    assert_equal [h, i, period], braille_input
+  end
+
+  def test_translate_of_capital_at_end_gives_braille_array
+    test = NightWriter.new
+    input = ["Hi."]
+
+    braille_input = test.translate(input)
+
+    capital = [".", ".", ".", ".", ".", "0"]
+    h = ["0", ".", "0", "0", ".", "."]
+    i = [".", "0", "0", ".", ".", "."]
+    period = [".", ".", "0", "0", ".","0"]
+
+    assert_equal [capital, h, i, period], braille_input
+  end
+
+  def test_translate_all_options_together
+    test = NightWriter.new
+    input = ["Hi 123 I."]
+
+    braille_input = test.translate(input)
+
+    capital = [".", ".", ".", ".", ".", "0"]
+    h = ["0", ".", "0", "0", ".", "."]
+    i = [".", "0", "0", ".", ".", "."]
+    space = [".", ".", ".", ".", ".", "."]
+    pound = [".", "0", ".", "0", "0", "0"]
+    one = ["0", ".", ".", ".", ".", "."]
+    two = ["0", ".", "0", ".", ".", "."]
+    three = ["0", "0", ".", ".", ".", "."]
+    period = [".", ".", "0", "0", ".","0"]
+
+    assert_equal [capital, h, i, space, pound, one, two, three, period, capital, i], braille_input
+  end
+
+  def test_braille_formatter_returns_three_lines_per_char
     test = NightWriter.new
 
-    letters = "H"
+    word = ["h"]
 
-    line_1 = [".", "0"]
-    line_2 = [".", "0"]
-    line_3 = ["0", "0"]
-
-    assert_equal line_1 && line_2 && line_3, test.non_number_translator(letters)
+    array = translate(word)
   end
+
+  def test_braille_joiner_creates_three_strings_from_multiple_chars
+    test = NightWriter.new
+
+    word = ["hi"]
+
+    array = translate(word)
+  end
+
 end
